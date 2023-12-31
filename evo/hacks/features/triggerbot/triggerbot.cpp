8c6ba2d601e6ab5ca48705ba190e332e0c998c69
Dutu1337 <46423190+dumitru1216@users.noreply.github.com>
2023-12-31 02:44:03 -0800

    a

--- evo/hacks/features/triggerbot/triggerbot.cpp
+++ evo/hacks/features/triggerbot/triggerbot.cpp
@@ -32,4 +32,49 @@ void evo::trigger_t::run_trigger( const evo::c_entity& local_entity ) {
 
 	/* store this shit here */
 	list_avaible_entries = _proc_manager.trace_address( _address->get_entity_list_address( ), { 0x8 * ( ent_handler >> 9 ) + 0x10,0x0 } );
+	if ( list_avaible_entries == 0 ) {
+#ifdef read_data_dbg
+		print_with_data_scoped( "trigger_t::run_trigger -> error -> idx 0 ( invalid ) [ 2 ]" );
+#endif // read_data_dbg
+		return;
+	}
+
+	if ( !_proc_manager.read_memory<DWORD64>( list_avaible_entries + 0x78 * ( ent_handler & 0x1FF ), entry_pawn_address ) ) {
+#ifdef read_data_dbg
+		print_with_data_scoped( "trigger_t::run_trigger -> error -> no memory [ 3 ]" );
+#endif // read_data_dbg
+		return;
+	}
+
+	/* update pawn */
+	if ( !entity.update_pawn( entry_pawn_address ) ) {
+#ifdef read_data_dbg
+		print_with_data_scoped( "trigger_t::run_trigger -> error -> pawn::handler_t [ 4 ]" );
+#endif // read_data_dbg
+		return;
+	}
+
+	/* add team check here */
+	allow_shot = entity.player_pawn.health > 0;
+
+	if ( !allow_shot ) {
+		/* we re not gonna log this */
+		return;
+	}
+
+	static std::chrono::time_point last_point = std::chrono::steady_clock::now( );
+	auto cur_point = std::chrono::steady_clock::now( );
+	if ( cur_point - last_point >= std::chrono::milliseconds( 30 ) ) {
+		const bool shooting = GetAsyncKeyState( VK_LBUTTON ) < 0;
+
+		/* run if !shoting */
+		if ( !shooting ) {
+			mouse_event( MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0 );
+			std::thread trigger_thread( release_mouse_event );
+			trigger_thread.detach( );
+		}
+
+		/* egalize these */
+		last_point = cur_point;
+	}
 }
