8db9a74a2f20adc227a4ec9f2016cd51c8e5df0a
Dutu1337 <46423190+dumitru1216@users.noreply.github.com>
2024-01-03 07:49:38 -0800

    a

--- evo/hacks/features/grenade/grenades.cpp
+++ evo/hacks/features/grenade/grenades.cpp
@@ -1,3 +1,5 @@
 #include "../../../inc.hpp"
 
-void evo::grenade_t::run( const c_entity& local_player, const c_entity& entity, int local_index, int index ) { }
+void evo::grenade_t::run( const c_entity& local_player, const c_entity& entity, int local_index, int index ) {
+
+}
